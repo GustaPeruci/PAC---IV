14b12c7c68b781a66030226ad09a5f5b17779c5a
DaniOrze <daniorzechowski@gmail.com>
2023-09-28 21:35:14 -0300

    fix: arrumada as telas de login e signup

--- snackbook/lib/pages/signup.dart
+++ snackbook/lib/pages/signup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:snackbook/colors.dart';
 
 class SignupPage extends StatefulWidget {
   const SignupPage({Key? key}) : super(key: key);
@@ -11,6 +12,7 @@ class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: background,
       body: SafeArea(
         child: ListView(
           padding: const EdgeInsets.symmetric(horizontal: 24.0),
@@ -27,6 +29,13 @@ class _SignupPageState extends State<SignupPage> {
                 TextField(
                   decoration: InputDecoration(
                     labelText: 'Nome Completo',
+                    labelStyle: TextStyle(color: kShrineGreen900),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: kShrineGreen900),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: kShrineGreen400),
+                    ),
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10.0),
                     ),
@@ -36,6 +45,13 @@ class _SignupPageState extends State<SignupPage> {
                 TextField(
                   decoration: InputDecoration(
                     labelText: 'Email',
+                    labelStyle: TextStyle(color: kShrineGreen900),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: kShrineGreen900),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: kShrineGreen400),
+                    ),
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10.0),
                     ),
@@ -45,6 +61,13 @@ class _SignupPageState extends State<SignupPage> {
                 TextField(
                   decoration: InputDecoration(
                     labelText: 'Celular',
+                    labelStyle: TextStyle(color: kShrineGreen900),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: kShrineGreen900),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: kShrineGreen400),
+                    ),
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10.0),
                     ),
@@ -54,6 +77,13 @@ class _SignupPageState extends State<SignupPage> {
                 TextField(
                   decoration: InputDecoration(
                     labelText: 'Nome de Usuário',
+                    labelStyle: TextStyle(color: kShrineGreen900),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: kShrineGreen900),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: kShrineGreen400),
+                    ),
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10.0),
                     ),
@@ -63,6 +93,13 @@ class _SignupPageState extends State<SignupPage> {
                 TextField(
                   decoration: InputDecoration(
                     labelText: 'Senha',
+                    labelStyle: TextStyle(color: kShrineGreen900),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: kShrineGreen900),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: kShrineGreen400),
+                    ),
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10.0),
                     ),
@@ -77,15 +114,38 @@ class _SignupPageState extends State<SignupPage> {
                       onPressed: () {
                         Navigator.of(context).pushNamed('/login');
                       },
-                      child: const Text('Já Possuo Conta'),
+                      child: const Text(
+                        'Já Possuo Conta',
+                        style: TextStyle(color: kShrineGreen50),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        elevation: 8.0,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        ),
+                        backgroundColor: kShrineGreen900,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10.0,
                     ),
                     ElevatedButton(
-                      onPressed: () {
-                      },
-                      child: const Text('Criar Conta'),
+                      onPressed: () {},
+                      child: const Text(
+                        'Criar Conta',
+                        style: TextStyle(color: kShrineGreen50),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        elevation: 8.0,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        ),
+                        backgroundColor: kShrineGreen900,
+                      ),
                     ),
                   ],
                 ),
+                const SizedBox(height: 16),
               ],
             ),
           ],
