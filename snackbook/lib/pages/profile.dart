3827f3ced9626e572390e7572b1e7e237cecc1e7
G_Raj <gustavosss06@gmail.com>
2023-10-14 18:36:42 -0300

    Desenvolvimento inicial da tela de perfil e edição de perfil
    - Criada a tela de perfil com acesso pela tela inicial que tem, foto do usuário, nome de usuário, celular do usuário, edição de perfil, receitas curtidas e postadas
    - Criada tela de edição de perfil com acesso pela tela de perfil, que terá as informações de usuário para editar como nome de usuário, celular e bio
    - Criado o ThemaData para decoração padrão dos campos de input

---
+++ snackbook/lib/pages/profile.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import '../colors.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Perfil'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 150,
+              width: 150,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: kSbGreen900,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Nome do usuário',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Nome completo',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Celular',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Biografia do usuário',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            SizedBox(height: 10),
+            TextButton(
+              style: TextButton.styleFrom(textStyle: TextStyle(fontSize: 16)),
+              onPressed: () {
+                Navigator.of(context).pushNamed('/editProfile');
+              },
+              child: const Text(
+                'Editar perfil',
+                style: TextStyle(color: kSbGreen900),
+              ),
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.favorite),
+                        color: kSbGreen900,
+                        onPressed: () {},
+                      ),
+                      Text(
+                        "Curtidas",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(width: 50),
+                      IconButton(
+                        icon: Icon(Icons.book),
+                        color: kSbGreen900,
+                        onPressed: () {},
+                      ),
+                      Text(
+                        "Posts",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
